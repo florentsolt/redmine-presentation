be30adae81d5ab1a3b3248aa3d0f47cda47093ed
Florent Solt <florent@solt.biz>
2014-05-20 13:45:07 +0200

    Remove extra empty <p>

--- assets/javascripts/run.js
+++ assets/javascripts/run.js
@@ -1,5 +1,10 @@
 $(function() {
   if ($('body.controller-wiki.action-show #content section').length > 0) {
+    var first_p = $('#content .wiki-page').children().first();
+    if (first_p.text() == '' && first_p.prop("tagName") == 'p') {
+      first.remove();
+    }
+
     var url = window.location.pathname + '/presentation';
     $("body.controller-wiki.action-show #content .contextual")
       .append($('<a>').attr('href', url).addClass('icon icon-test').text('Presentation'));
