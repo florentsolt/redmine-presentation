13c1d22ac50caab4d494c2479e59c794c0c95f03
Florent Solt <florent@solt.biz>
2015-05-21 14:13:44 +0200

    Fix routes for Redmine3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 RedmineApp::Application.routes.draw do
-  match '/projects/:project_id/wiki/:id/presentation', :to => 'wiki#presentation'
+  match '/projects/:project_id/wiki/:id/presentation', :to => 'wiki#presentation', :via => :get
 end
